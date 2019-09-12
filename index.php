bbe9d244f6066d15972d4081ac04a522e2bf3cca
Marcel Möller <marcelmoeller99@gmx.de>
2019-09-12 11:28:55 +0200

    Add simple example in index.php which runs through all available states

--- index.php
+++ index.php
@@ -3,6 +3,10 @@ define('BASEPATH', realpath(dirname(__FILE__)));
 require_once(BASEPATH . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
 
 $jedi = new Jedi();
-
+$jedi->run();
+$jedi->attack();
+$jedi->attack();
+$jedi->run();
+$jedi->attack();
 ?>
 
